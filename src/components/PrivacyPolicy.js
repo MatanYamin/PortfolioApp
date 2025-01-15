85a710c535e832c3af68f8144978e9ff8650dde5
Matan Yamin <yamin2211@gmail.com>
2025-01-15 14:34:04 +0200

    Changes in Privacy policy and Terms

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -3,79 +3,77 @@ import '../App.css';
 
 const PrivacyPolicy = () => {
   return (
-    <div id="about" className="container py-5">
+    <div id="privacyPolicy" className="container py-5">
       <div className="row">
         <div id="enlargedText" className="col-lg-15 col-xm-12">
           <h1 className="about-heading">Privacy Policy</h1>
           <p>
-myapps. built the TV Remote Control app as a Free app. This SERVICE is provided by myapps. at no cost and is intended for use as is.
-
-This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.
-
-If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.
-
-The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at TV Remote Control unless otherwise defined in this Privacy Policy.
-<br/><br/>
-<h4>Information Collection and Use</h4>
-For a better experience, while using our Service, I may require you to provide us with certain personally identifiable information. The information that I request will be retained on your device and is not collected by me in any way.
-
-The app does use third-party services that may collect information used to identify you.
-
-Link to the privacy policy of third-party service providers used by the app
-<br/>
-<ul>
-<li> 
-<a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a>
-</li>
-<li> 
-<a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank" rel="noopener noreferrer">AdMob</a>
-</li>
-<li> 
-<a href="https://firebase.google.com/support/privacy" target="_blank" rel="noopener noreferrer">Google Analytics for Firebase</a>
-</li>
-<li> 
-<a href="https://firebase.google.com/support/privacy/" target="_blank" rel="noopener noreferrer">Firebase Crashlytics</a>
-</li> 
-</ul>
-
-I want to inform you that whenever you use my Service, in a case of an error in the app I collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.
-<br/><br/>
-<h4>Cookies</h4>
-Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
-
-This Service does not use these “cookies” explicitly. However, the app may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
-<br/><br/>
-<h4>Service Providers</h4>
-
-I may employ third-party companies and individuals due to the following reasons:
-
-To facilitate our Service;
-To provide the Service on our behalf;
-To perform Service-related services; or
-To assist us in analyzing how our Service is used.
-I want to inform users of this Service that these third parties have access to their Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.
-<br/><br/>
-<h4>Security</h4>
-I value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its absolute security.
-
-Links to Other Sites
-
-This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by me. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
-<br/><br/>
-<h4>Children’s Privacy</h4>
-
-These Services do not address anyone under the age of 13. I do not knowingly collect personally identifiable information from children under 13 years of age. In the case I discover that a child under 13 has provided me with personal information, I immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact me so that I will be able to do the necessary actions.
-
-Changes to This Privacy Policy
-
-I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on this page.
-
-This policy is effective as of 2024-02-19
-
-<br/><br/>
-<h4>Contact Us</h4>
-If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at matanyamindevelop@gmail.com.
-            
+            <strong>Effective Date:</strong> February 19, 2024
+            <br/><br/>
+            <h4>Introduction</h4>
+            myapps. ("we," "our," or "us") develops and provides various applications and digital services. This Privacy Policy explains how we collect, use, disclose, and protect user information when you use our applications and services.
+            <br/><br/>
+            By using our applications, you consent to the collection and use of your information as described in this Privacy Policy.
+            <br/><br/>
+            <h4>Information Collection and Use</h4>
+            For a better experience, we may require certain personally identifiable information, which will be retained on your device and not collected by us directly.
+            <br/><br/>
+            Our applications use third-party services that may collect information to identify you. Below are the third-party services we use:
+            <br/>
+            <ul>
+              <li><a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a></li>
+              <li><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank" rel="noopener noreferrer">AdMob</a></li>
+              <li><a href="https://firebase.google.com/support/privacy" target="_blank" rel="noopener noreferrer">Google Analytics for Firebase</a></li>
+              <li><a href="https://firebase.google.com/support/privacy/" target="_blank" rel="noopener noreferrer">Firebase Crashlytics</a></li>
+            </ul>
+            Additionally, in the case of an app error, we collect Log Data (via third-party services) that may include:
+            <ul>
+              <li>Device IP address</li>
+              <li>Device name</li>
+              <li>Operating system version</li>
+              <li>App configuration details</li>
+              <li>Date and time of service use</li>
+              <li>Other diagnostic data</li>
+            </ul>
+            <br/><br/>
+            <h4>Cookies</h4>
+            Our applications do not use cookies explicitly. However, third-party services may use cookies to enhance their performance. You have the option to accept or refuse these cookies.
+            <br/><br/>
+            <h4>Third-Party Services</h4>
+            We may employ third-party companies and individuals for the following purposes:
+            <ul>
+              <li>Facilitating our Service</li>
+              <li>Providing the Service on our behalf</li>
+              <li>Performing service-related tasks</li>
+              <li>Assisting in Service analytics</li>
+            </ul>
+            These third parties have access to your personal information to perform assigned tasks but are obligated not to disclose or misuse it.
+            <br/><br/>
+            <h4>Security</h4>
+            We value your trust in providing personal information and strive to use commercially acceptable means to protect it. However, no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
+            <br/><br/>
+            <h4>Links to Other Sites</h4>
+            Our services may contain links to third-party websites. We are not responsible for the privacy practices of external sites and encourage you to review their privacy policies.
+            <br/><br/>
+            <h4>Children’s Privacy</h4>
+            Our applications are not intended for children under 13. We do not knowingly collect personal information from children under 13. If you believe a child has provided us with personal data, please contact us so we can delete it.
+            <br/><br/>
+            <h4>Your Rights (GDPR & CCPA Compliance)</h4>
+            Depending on your location, you may have rights regarding your personal data, including:
+            <ul>
+              <li>The right to access and update your data</li>
+              <li>The right to request deletion of your data</li>
+              <li>The right to opt out of targeted advertising</li>
+            </ul>
+            If you wish to exercise any of these rights, please contact us.
+            <br/><br/>
+            <h4>Changes to This Privacy Policy</h4>
+            We may update this Privacy Policy from time to time. Changes will be posted on this page, and continued use of our services constitutes acceptance of the updated policy.
+            <br/><br/>
+            <h4>Contact Us</h4>
+            If you have any questions or concerns about this Privacy Policy, you can reach us at:
+            <br/>
+            Email: <a href="mailto:matanyamindevelop@gmail.com">matanyamindevelop@gmail.com</a>
           </p>
         </div>
       </div>
@@ -83,4 +81,5 @@ If you have any questions or suggestions about my Privacy Policy, do not hesitat
   )
 }
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
+
--- src/components/Terms.js
+++ src/components/Terms.js
@@ -8,57 +8,54 @@ const Terms = () => {
         <div id="enlargedText" className="col-lg-15 col-xm-12">
           <h1 className="about-heading">Terms & Conditions</h1>
           <p>
-          By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages or make derivative versions. The app itself, and all the trademarks, copyright, database rights, and other intellectual property rights related to it, still belong to myapps.
-
-          myapps. is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.
-
-          The TV Remote Control app stores and processes personal data that you have provided to us, to provide my Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the TV Remote Control app won’t work properly or at all.
-
-          The app does use third-party services that declare their Terms and Conditions.
-
-          Link to Terms and Conditions of third-party service providers used by the app
-
-<br/>
-<ul>
-<li> 
-<a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a>
-</li>
-<li> 
-<a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank" rel="noopener noreferrer">AdMob</a>
-</li>
-<li> 
-<a href="https://firebase.google.com/support/privacy" target="_blank" rel="noopener noreferrer">Google Analytics for Firebase</a>
-</li>
-<li> 
-<a href="https://firebase.google.com/support/privacy/" target="_blank" rel="noopener noreferrer">Firebase Crashlytics</a>
-</li> 
-</ul>
-
-You should be aware that there are certain things that myapps. will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi or provided by your mobile network provider, but myapps. cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.
-
-If you’re using the app outside of an area with Wi-Fi, you should remember that the terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app, or other third-party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app.
-
-Along the same lines, myapps. cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, myapps. cannot accept responsibility.
-
-With respect to myapps. responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavor to ensure that it is updated and correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. myapps. accepts no liability for any loss, direct or indirect, you experience as a result of relying wholly on this functionality of the app.
-
-At some point, we may wish to update the app. The app is currently available on Android – the requirements for the system(and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. myapps. does not promise that it will always update the app so that it is relevant to you and/or works with the Android version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device.
-
-<br/><br/>
-
-<h4>Changes to This Terms and Conditions</h4>
-
-I may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Terms and Conditions on this page.
-
-These terms and conditions are effective as of 2024-02-19
-
-<br/><br/>
-<h4>Contact Us</h4>
-If you have any questions or suggestions about my Terms and Conditions, do not hesitate to contact me at matanyamindevelop@gmail.com.          </p>
+          <strong>Effective Date:</strong> February 19, 2024
+          <br/><br/>
+          <h4>Introduction</h4>
+          myapps. ("we," "our," or "us") provides various applications and digital services. These Terms and Conditions govern your use of our applications and services. By downloading or using any of our applications, you agree to these Terms and Conditions.
+          <br/><br/>
+          <h4>Use of the App</h4>
+          You are not permitted to copy, modify, distribute, sell, or lease any part of our applications or services. You may not attempt to extract the source code, translate the application into other languages, or create derivative versions.
+          <br/><br/>
+          The app and all related trademarks, copyrights, database rights, and other intellectual property rights remain the property of myapps.
+          <br/><br/>
+          <h4>Modifications and Charges</h4>
+          We reserve the right to modify or discontinue our applications or services at any time. If we introduce any charges for our applications or features, we will clearly notify users before any payments are required.
+          <br/><br/>
+          <h4>Data and Security</h4>
+          Our applications may store and process personal data you provide. It is your responsibility to secure your device and account credentials. We strongly advise against jailbreaking or rooting your device, as this may affect the app's functionality and security.
+          <br/><br/>
+          <h4>Third-Party Services</h4>
+          Our applications utilize third-party services, which have their own terms and conditions:
+          <br/>
+          <ul>
+            <li><a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a></li>
+            <li><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank" rel="noopener noreferrer">AdMob</a></li>
+            <li><a href="https://firebase.google.com/support/privacy" target="_blank" rel="noopener noreferrer">Google Analytics for Firebase</a></li>
+            <li><a href="https://firebase.google.com/support/privacy/" target="_blank" rel="noopener noreferrer">Firebase Crashlytics</a></li>
+          </ul>
+          <br/><br/>
+          <h4>Connectivity and Service Availability</h4>
+          myapps. is not responsible for issues arising from lack of internet connectivity, mobile data charges, or device power limitations affecting app performance.
+          <br/><br/>
+          <h4>Liability Disclaimer</h4>
+          We endeavor to ensure our applications are accurate and functional but are not responsible for errors, service interruptions, or third-party data reliance.
+          <br/><br/>
+          <h4>Updates and Termination</h4>
+          We may update our applications over time, and you agree to install such updates to continue using our services. We also reserve the right to discontinue support for any application or service without prior notice.
+          <br/><br/>
+          <h4>Changes to These Terms</h4>
+          We may update these Terms and Conditions periodically. Updates will be posted on this page, and continued use of our applications constitutes acceptance of the revised terms.
+          <br/><br/>
+          <h4>Contact Us</h4>
+          If you have any questions or concerns about these Terms and Conditions, please contact us at:
+          <br/>
+          Email: <a href="mailto:matanyamindevelop@gmail.com">matanyamindevelop@gmail.com</a>
+          </p>
         </div>
       </div>
     </div>
   )
 }
 
-export default Terms
+export default Terms;
+
